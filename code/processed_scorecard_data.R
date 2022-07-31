2c78c01871dc0e5bab6bb831a72ac923c266cb8d
Brad <bstirling@seattleu.edu>
2022-07-31 15:16:44 -0700

    Updated logic for high vs low school earnings

--- code/processed_scorecard_data.R
+++ code/processed_scorecard_data.R
@@ -10,20 +10,19 @@ trends <- map_df(trends_vec, read_csv) %>%
 trends <- trends %>% mutate(date_wk_s = str_sub(monthorweek, 1, 10))
 trends <- trends %>% mutate(date_wk_s = ymd(date_wk_s))
 trends <- trends %>%
-  filter( date_wk_s > ymd(20140831) & date_wk_s < ymd(20150328) | date_wk_s > ymd(20150831)) # Compares sep14 to mar15 and sep15 to mar16
+  filter( date_wk_s > ymd(20140831) & date_wk_s < ymd(20150328) | date_wk_s > ymd(20150831)) # Compares Sep 14 to Mar 15 and Sep15 to Mar 16
 trends <- trends %>% mutate(sc_implmnt = date_wk_s >= ymd(20150901))
 trends <- trends %>% group_by(schname, keyword) %>%
   mutate(ind_mean = mean(index, na.rm = TRUE))
 trends <- trends %>% group_by(schname, keyword) %>%
   mutate(ind_std = sd(index)) %>% ungroup()
-  
+
 trends %>% ggplot(aes(ind_std)) + geom_histogram(bins = 100) # Check if z-scores follow normal distribution
 
 trends <- trends %>% mutate(ind_stand = (index - ind_mean)/ind_std) # Standardize rankings
-
-trends <- trends %>% group_by(schname, sc_implmnt) %>% summarize(ind_z = mean(ind_stand))
+trends <- trends %>% group_by(schname, sc_implmnt) %>% summarize(ind_z = mean(ind_stand)) # Calculate mean z-scores for pre and post scorecard launch
 trends <- trends %>% spread(sc_implmnt, ind_z) %>% rename(ind_z_pre = "FALSE", ind_z_post = "TRUE")
-trends <- trends %>% mutate(ind_z_shift = ind_z_post - ind_z_pre)
+trends <- trends %>% mutate(ind_z_shift = ind_z_post - ind_z_pre) # Calculate z-score ranking shift following scorecard launch
 
 ## Id link table clean
 id_name_link <- read_csv("data/id_name_link.csv")
@@ -40,24 +39,11 @@ scorecard <- scorecard %>%
   mutate(`med_earnings_10yrs` = as.numeric(`med_earnings_10yrs`)) %>%
   drop_na(`med_earnings_10yrs`)
 
-quantile(scorecard$`med_earnings_10yrs`)
-
-scorecard <- scorecard %>% mutate(earn_cat = case_when(`med_earnings_10yrs` >
-                                    quantile(scorecard$`med_earnings_10yrs`)[4] ~  "high",
-                                  `med_earnings_10yrs` <
-                                  quantile(scorecard$`med_earnings_10yrs`)[2] ~ "low",
-                                  TRUE ~ "mid")
-                                  ) %>% filter(earn_cat != "mid") %>% 
-  mutate(across(c(LOCALE, CONTROL), as.factor))
-
+scorecard <- scorecard %>% mutate(across(c(LOCALE, CONTROL), as.factor))
 scorecard$CONTROL <- recode_factor(scorecard$CONTROL,
                                          "1" = "Public",
                                          "2" = "PrivateNonProfit",
                                          "3" = "PrivateForProfit")
-
-
-scorecard <- scorecard %>% mutate(earn_cat = case_when(earn_cat == "high" ~ 1,
-                                                       TRUE ~ 0)) %>% rename(high_earning = earn_cat )
 
 scorecard <- scorecard %>% mutate(across(c(SAT_AVG, UGDS, GRAD_DEBT_MDN_SUPP, UG25abv), as.numeric))
 scorecard <- scorecard %>% mutate(across(contains("RET"), as.numeric))
@@ -77,6 +63,19 @@ scorecard <- scorecard %>% mutate (prgm_prct_stem = rowSums(across(c("PCIP10",
 scorecard_f <- scorecard %>%
   inner_join(id_name_link, by = c("UNITID" = "unitid", "OPEID" = "opeid")) %>%
   inner_join(trends, by = "schname") %>% subset(select = -schname)
+
+quantile(scorecard_f$`med_earnings_10yrs`)
+
+scorecard_f <- scorecard_f %>% mutate(earn_cat = case_when(`med_earnings_10yrs` >
+                                                         quantile(scorecard_f$`med_earnings_10yrs`)[4] ~  "high",
+                                                       `med_earnings_10yrs` <
+                                                         quantile(scorecard_f$`med_earnings_10yrs`)[2] ~ "low",
+                                                       TRUE ~ "mid")) %>% filter(earn_cat != "mid")
+
+
+scorecard_f <- scorecard_f %>% mutate(earn_cat = case_when(earn_cat == "high" ~ 1,
+                                                       TRUE ~ 0)) %>% rename(high_earning = earn_cat)
+
 
 save(scorecard_f, file =  "processed_data/processed_scorecard.RData")
 
