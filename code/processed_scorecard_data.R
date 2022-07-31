c090c34f8a0280874216f28a804fbe5ffef1c6f2
Brad <bstirling@seattleu.edu>
2022-07-30 17:38:57 -0700

    Updated write function to csv

--- code/processed_scorecard_data.R
+++ code/processed_scorecard_data.R
@@ -82,5 +82,5 @@ scorecard_f <- scorecard %>%
   inner_join(id_name_link, by = c("UNITID" = "unitid", "OPEID" = "opeid")) %>%
   inner_join(trends, by = "schname") %>% subset(select = -schname)
 
-save(scorecard_f, file = "processed_data/processed_scorecard.RData")
+write_csv(scorecard_f, "processed_data/processed_scorecard.csv")
 
